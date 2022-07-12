f8eadec9dbd3198cf3c8544c5b5724a3c2f4f685
UmerZer0 <uno4umer@gmail.com>
2022-07-12 13:34:25 +0500

    @UmerZer0

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ from collections import Counter
 jsonFile = open('watch-history.json', encoding="utf8")
 
 data = json.load(jsonFile)
+
+#! Channels List
 initialList = []
 
 for i in data:
@@ -17,5 +19,24 @@ result = [
 ]
 
 myFile = open("Channels List.txt", "w",  encoding="utf-8")
+myFile.write("\n".join(map(str, result)))
+myFile.close()
+
+
+#! Videos List
+initialList = []
+
+for i in data:
+    tempTitle = i.get('title')
+    if tempTitle != None:
+        initialList.append(str(tempTitle))
+
+result = [
+    item + ' count: ' + str(c) 
+    for item, c in Counter(initialList).most_common()
+    if(item != "Watched a video that has been removed")
+]
+
+myFile = open("Videos List.txt", "w",  encoding="utf-8")
 myFile.write("\n".join(map(str, result)))
 myFile.close()
